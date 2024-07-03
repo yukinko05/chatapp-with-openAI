120374a1e6718ce91e67d6ed1dcf770b866d6a6b
Yukina <grey.sora.35@gmail.com>
2024-07-03 19:51:18 +0900

    メールアドレスとパスワードのバリデーションチェックの実装

--- chatapplication-with-chatgpt/src/app/auth/register/page.tsx
+++ chatapplication-with-chatgpt/src/app/auth/register/page.tsx
@@ -1,17 +1,56 @@
-import React from "react";
+"use client"
 
-const page = () => {
+import React from "react";
+import {SubmitHandler, useForm} from "react-hook-form";
+
+type Inputs = {
+    email: string;
+    password: string;
+}
+const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        console.log(data);
+    }
+
     return (
         <div className="h-screen flex flex-col items-center justify-center">
-            <form className="bg-white p-8 rounded-lg shadow-md w-96">
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="bg-white p-8 rounded-lg shadow-md w-96">
                 <h1 className="mb-4 text-2xl text-gray-700 font-medium">新規登録</h1>
                 <div className="mb-4">
                     <label className="block  text-sm font-medium text-gray-600">Email</label>
-                    <input type="text" className="mt-1 border-2 rounded-md w-full p-2"/>
+                    <input
+                        {...register("email", {
+                            required: "メールアドレスは必須です。",
+                            pattern: {
+                                value: /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+                                message: "不適切なメールアドレスです。"
+                            }
+                        })} type="text"
+                        className="mt-1 border-2 rounded-md w-full p-2"
+                    />
+                    {errors.email && <span className="text-red-700">{errors.email.message}</span>}
                 </div>
                 <div className="mb-4">
                     <label className="block  text-sm font-medium text-gray-600">Password</label>
-                    <input type="password" className="mt-1 border-2 rounded-md w-full p-2"/>
+                    <input
+                        {...register("password", {
+                            required: "パスワードは必須です。",
+                            minLength: {
+                                value: 6,
+                                message: "6文字上入力してください。"
+                            }
+                        })}
+                        type="password" className="mt-1 border-2 rounded-md w-full p-2"
+                    />
+                    {errors.password && <span className="text-red-700">{errors.password.message}</span>}
                 </div>
 
                 <div className="flex justify-end">
@@ -34,4 +73,4 @@ const page = () => {
     )
 };
 
-export default page;
+export default Register;
