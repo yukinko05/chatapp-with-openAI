142985aff4a263fbbb2b8d2ab25c7c8736377607
Yukina <grey.sora.35@gmail.com>
2024-07-04 19:53:27 +0900

    Firebaseからデータを取得する非同期処理を実装

--- chatapplication-with-chatgpt/src/app/components/sidebar.tsx
+++ chatapplication-with-chatgpt/src/app/components/sidebar.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client"
+
+import React, {useEffect} from "react";
 import {RiLogoutBoxLine} from "react-icons/ri";
+import {collection, onSnapshot, orderBy, query} from "@firebase/firestore";
+import {db} from "../../../firebase";
 
 const Sidebar = () => {
+
+    useEffect(() => {
+
+        const fetchRooms = async () => {
+            const roomCollectionRef = collection(db, "rooms");
+            const q = query(roomCollectionRef, orderBy("createdAt"));
+            const unsubscribe = onSnapshot(q, (snapshot) => {
+                const rewRooms = snapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        ...doc.data(),
+                    }));
+                console.log(rewRooms);
+            });
+        };
+        fetchRooms();
+
+    }, []);
+
     return (
         <div className="bg-custom-blue h-full overflow-y-auto px-5 flex flex-col">
             <div className="flex-grow">
@@ -23,7 +45,8 @@ const Sidebar = () => {
                 </ul>
             </div>
 
-            <div className=" flex items-center justify-evenly mb-2 cursor-pointer p-4 text-slate-100 hover:bg-slate-700">
+            <div
+                className=" flex items-center justify-evenly mb-2 cursor-pointer p-4 text-slate-100 hover:bg-slate-700">
                 <RiLogoutBoxLine/>
                 <span>ログアウト</span>
             </div>
