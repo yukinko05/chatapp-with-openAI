651914f236c32fe6861549edbed578ec836e3a05
Yukina <grey.sora.35@gmail.com>
2024-07-04 18:51:14 +0900

    Chatコンポーネント作成

--- chatapplication-with-chatgpt/src/app/components/sidebar.tsx
+++ chatapplication-with-chatgpt/src/app/components/sidebar.tsx
@@ -1,27 +1,33 @@
 import React from "react";
+import {RiLogoutBoxLine} from "react-icons/ri";
 
 const Sidebar = () => {
     return (
-    <div className="bg-custom-blue h-full overflow-y-auto px-5 flex-col">
-        <div className="flex-grow">
-            <div className="cursor-pointer flex justify-evenly items-center border mt-2 rounded-md hover:bg-blue-800 duration-150">
-                <span className="text-white p-4 text-2x1">＋</span>
-                <h1 className="text-white texd-xl font-semibold p-4">New Chat</h1>
+        <div className="bg-custom-blue h-full overflow-y-auto px-5 flex flex-col">
+            <div className="flex-grow">
+                <div
+                    className="cursor-pointer flex justify-evenly items-center border mt-2 rounded-md hover:bg-blue-800 duration-150">
+                    <span className="text-white p-4 text-2x1">＋</span>
+                    <h1 className="text-white texd-xl font-semibold p-4">New Chat</h1>
+                </div>
+                <ul>
+                    <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
+                        Room 1
+                    </li>
+                    <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
+                        Room 2
+                    </li>
+                    <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
+                        Room 3
+                    </li>
+                </ul>
             </div>
 
+            <div className=" flex items-center justify-evenly mb-2 cursor-pointer p-4 text-slate-100 hover:bg-slate-700">
+                <RiLogoutBoxLine/>
+                <span>ログアウト</span>
+            </div>
         </div>
-        <ul>
-            <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
-                Room 1
-            </li>
-            <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
-                Room 2
-            </li>
-            <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
-                Room 3
-            </li>
-        </ul>
-    </div>
     );
 };
 
