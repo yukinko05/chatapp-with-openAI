3a88269308bd52adee1fa9c12b43fc1e86910f9f
Yukina <grey.sora.35@gmail.com>
2024-07-07 18:05:49 +0900

    OpenAIから返信を受け取る実装完了

--- chatapplication-with-chatgpt/src/app/components/Chat.tsx
+++ chatapplication-with-chatgpt/src/app/components/Chat.tsx
@@ -5,6 +5,7 @@ import {FaPaperPlane} from "react-icons/fa6";
 import {addDoc, collection, doc, onSnapshot, orderBy, query, serverTimestamp, Timestamp} from "@firebase/firestore";
 import {db} from "../../../firebase";
 import {useAppContext} from "@/context/AppContext";
+import OpenAI from "openai";
 
 type Message = {
     text: string;
@@ -13,6 +14,12 @@ type Message = {
 }
 
 const Chat = () => {
+
+    const openai = new OpenAI({
+        apiKey: process.env.NEXT_PUBLIC_OPEN_KEY,
+        dangerouslyAllowBrowser:true,
+    });
+
     const {selectedRoom} = useAppContext();
     const [inputMessage, setInputMessage] = useState<string>("");
     const [messages, setMessages] = useState<Message[]>([]);
@@ -20,15 +27,14 @@ const Chat = () => {
     useEffect(() => {
         if (selectedRoom) {
             const fetchMessages = async () => {
-                const roomDomRef = doc(db, "rooms", selectedRoom);
-                const messagesCollectionRaf = collection(roomDomRef, "messages");
+                const roomDocRef = doc(db, "rooms", selectedRoom);
+                const messagesCollectionRaf = collection(roomDocRef, "messages");
 
                 const q = query(messagesCollectionRaf, orderBy("createdAt"));
 
                 const unsubscribe = onSnapshot(q, (snapshot) => {
                     const newMessages = snapshot.docs.map((doc) => doc.data() as Message);
                     setMessages(newMessages);
-                    console.log(newMessages);
                 });
 
                 return () => {
@@ -39,19 +45,34 @@ const Chat = () => {
         }
     }, [selectedRoom]);
 
+
     const sendMessage = async () => {
         if (!inputMessage.trim()) return;
 
         const messageData = {
             text: inputMessage,
             sender: "user",
-            createdAd: serverTimestamp(),
+            createdAt: serverTimestamp(),
         };
 
-        const roomRef = doc(db, "rooms", "OhjTqIauzDGku1MOnrRv");
+        //メッセージをFirebaseに保存
+        const roomRef = doc(db, "rooms", selectedRoom!);
         const messageCollectionRef = collection(roomRef, "messages");
         await addDoc(messageCollectionRef, messageData);
-    }
+
+        //OpenAIからの返信
+        const gpt3Response = await openai.chat.completions.create({
+            messages: [{ role: "user", content: inputMessage }],
+            model:"gpt-3.5-turbo",
+        });
+
+        const botResponse = gpt3Response.choices[0].message.content;
+        await addDoc(messageCollectionRef, {
+            text: botResponse,
+            sender: "bot",
+            createdAt: serverTimestamp(),
+        })
+    };
     return (
         <div className="bg-gray-500 h-full p-4 flex flex-col">
             <h1 className="text-2xl text-white font-semibold mb-4">
@@ -60,7 +81,7 @@ const Chat = () => {
             <div className="flex-grow overflow-y-auto mb-4">
                 {messages.map((message, index) => (
                     <>
-                        <div key={index} className={message.sender === "user" ? "text-right" : "text-left"}>
+                        <div　key={index} className={message.sender === "user" ? "text-right" : "text-left"}>
                             <div
                                 className={message.sender === "user" ? "bg-blue-500 inline-block rounded px-4 py-2 mb-2"
                                     : "bg-green-500 inline-block rounded px-4 py-2 mb-2"
@@ -69,21 +90,6 @@ const Chat = () => {
                                 <p className="text-white font-medium">{message.text}</p>
                             </div>
                         </div>
-
-                        {/*<div className="text-right">*/}
-                        {/*    <div className="bg-blue-500 inline-block rounded px-4 py-2 mb-2">*/}
-                        {/*        <p className="text-white font-medium">*/}
-                        {/*            {message.text}*/}
-                        {/*        </p>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
-                        {/*<div className="text-left">*/}
-                        {/*    <div className="bg-green-500 inline-block rounded px-4 py-2 mb-2">*/}
-                        {/*        <p className="text-white font-medium">*/}
-                        {/*            {message.text}*/}
-                        {/*        </p>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
                     </>
                 ))}
             </div>
@@ -94,6 +100,7 @@ const Chat = () => {
                     placeholder="Send a Message"
                     className="border-2 rounded w-full pr-10 focus:outline-none p-2"
                     onChange={(e) => setInputMessage(e.target.value)}
+                    value={inputMessage}
                 />
                 <button
                     className="absolute inset-y-0 right-4 flex items-center"
